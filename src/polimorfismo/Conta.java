daa3f0383907030131e006b40771d6289f8ff023
Anderson Luis Ribeiro <anderson.luis.rib@gmail.com>
2012-09-06 18:57:04 -0300

    Resolvendo conflitos...

--- src/polimorfismo/Conta.java
+++ src/polimorfismo/Conta.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -7,50 +7,7 @@ package polimorfismo;
 
 /**
  *
- * @author PRINCIPAL
- */
-public class Conta {
-    
-    
-    protected Double saldo;    
-    
-    /**
-     * Aumenta o saldo com a quantia depositada
-     * @param quantia 
-     */
-    public void deposita(double quantia){
-        this.saldo += quantia;
-        
-    }
-    
-    /**
-     * Diminui o saldo com a quantia sacada
-     * @param quantia 
-     */
-    public void saca(double quantia){
-        this.saldo -= quantia;
-        
-    }
-    
-    /**
-     * Diminui o saldo com a ataxa fornecida
-     * @param taxa 
-     */
-    public void atualiza(double taxa){
-        this.saldo -= taxa;
-    }
-    
-}
-=======
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package polimorfismo;
-
-/**
- *
- * @author PRINCIPAL
+ * @author Anderson
  */
 public class Conta {
     
@@ -81,4 +38,4 @@ public class Conta {
     }
     
 }
->>>>>>> 9e9ad21f54ecba72b72a1a2c30b8f9ab2bcf7cf7
+
