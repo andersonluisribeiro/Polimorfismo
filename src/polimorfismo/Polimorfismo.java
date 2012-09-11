823058f6df1bc392a8156242e7fe2a6d7e313831
rafaelpadua <rafael.padua.src@gmail.com>
2012-09-11 13:43:42 -0300

    criando alteracoes

--- src/polimorfismo/Polimorfismo.java
+++ src/polimorfismo/Polimorfismo.java
@@ -14,6 +14,7 @@ public class Polimorfismo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+         Conta conta = new ContaSalario();
+         System.out.println(conta.saldo);
     }
 }
